00868ff4abeee918fbbabeb3d5508c346a8631c2
leonarddepaula <leodepaula315@gmail.com>
2024-04-06 11:16:19 -0300

    up project

---
+++ apps/bolsa-valores/src/orders/orders.controller.ts
@@ -0,0 +1,63 @@
+import { Controller } from '@nestjs/common';
+import { GrpcMethod, MessagePattern, Payload } from '@nestjs/microservices';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @GrpcMethod('OrderService')
+  async createOrder(@Payload() createOrderDto: CreateOrderDto) {
+    console.log(createOrderDto);
+    const order = await this.ordersService.create(createOrderDto);
+    return {
+      order: {
+        order_id: order.id.toString(),
+        account_id: order.account_id,
+        asset_id: order.asset_id,
+        quantity: order.quantity,
+        status: order.status,
+      },
+    };
+  }
+
+  @GrpcMethod('OrderService')
+  async findAllOrders(@Payload() finAllOrdersDto: { account_id: string }) {
+    const orders = await this.ordersService.findAll(finAllOrdersDto.account_id);
+    return {
+      orders: orders.map((order) => ({
+        order_id: order.id.toString(),
+        account_id: order.account_id,
+        asset_id: order.asset_id,
+        quantity: order.quantity,
+        status: order.status,
+      })),
+    };
+  }
+
+  @GrpcMethod('OrderService')
+  async findOneOrder(@Payload() findOneOrderDto: { order_id: string }) {
+    const order = await this.ordersService.findOne(findOneOrderDto.order_id);
+    return {
+      order: {
+        order_id: order.id.toString(),
+        account_id: order.account_id,
+        asset_id: order.asset_id,
+        quantity: order.quantity,
+        status: order.status,
+      },
+    };
+  }
+
+  @MessagePattern('updateOrder')
+  update(@Payload() updateOrderDto: UpdateOrderDto) {
+    return this.ordersService.update(updateOrderDto.id, updateOrderDto);
+  }
+
+  @MessagePattern('removeOrder')
+  remove(@Payload() id: number) {
+    return this.ordersService.remove(id);
+  }
+}
---
+++ apps/nestjs-grpc/src/orders/orders.service.ts
@@ -0,0 +1,79 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { Client, ClientGrpc, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { Metadata } from '@grpc/grpc-js';
+import { lastValueFrom, Observable } from 'rxjs';
+
+type Order = {
+  order_id: string;
+  account_id: string;
+  asset_id: string;
+  quantity: number;
+  status: string;
+};
+
+interface OrderGrpcClient {
+  createOrder(
+    data: {
+      account_id: string;
+      asset_id: string;
+      quantity: number;
+    },
+    metadata?: Metadata,
+  ): Observable<{
+    order: Order;
+  }>;
+  findAllOrders(
+    data: { account_id: any },
+    metadata?: Metadata,
+  ): Observable<{ orders: Order[] }>;
+  findOneOrder(
+    data: { order_id: string },
+    metadata?: Metadata,
+  ): Observable<{ order: Order }>;
+}
+
+@Injectable()
+export class OrdersService implements OnModuleInit {
+  @Client({
+    transport: Transport.GRPC,
+    options: {
+      package: 'com.dev',
+      protoPath: [join(__dirname, 'orders', 'proto', 'orders.proto')],
+      loader: { keepCase: true },
+    },
+  })
+  clientGrpc: ClientGrpc;
+
+  private orderGrpcClient: OrderGrpcClient;
+
+  onModuleInit() {
+    this.orderGrpcClient = this.clientGrpc.getService('OrderService');
+  }
+  async create(createOrderDto: CreateOrderDto) {
+    const result = await lastValueFrom(this.orderGrpcClient.createOrder(createOrderDto));
+    return result.order
+  }
+  async findAll(account_id: string) {
+    const result = await lastValueFrom(
+      this.orderGrpcClient.findAllOrders({ account_id }),
+    );
+    return result.orders;
+  }
+  async findOne(id: string) {
+    const result = await lastValueFrom(
+      this.orderGrpcClient.findOneOrder({ order_id: id }),
+    );
+    return result.order;
+  }
+
+  update(id: string, updateOrderDto: UpdateOrderDto) {
+    return `This action updates a #${id} order`;
+  }
+
+  remove(id: string) {
+    return `This action removes a #${id} order`;
+  }
+}
